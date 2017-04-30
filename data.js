619b2c8f672538cd7839a509f6791cb55cb9df87
diewland <diewland@gmail.com>
2017-04-30 22:36:35 +0700

    hello kb2

---
+++ data.js
@@ -0,0 +1,38 @@
+var data = [
+  {
+    color:    'blue',
+    type:     'Tactile Click',
+    force_g:  50,
+    info:     'The MX Blue switch is usually reminds people of the old "clicky" keyboards from the early PC days. The combination of the strong tactile feedback and simultaneous click gives a very satisfying feel while typing. However, the clicking can be loud, which can limit its use in shared computing spaces.',
+  },
+  {
+    color:    'green',
+    type:     'Tactile Click',
+    force_g:  70,
+    info:     'The MX Green switches are almost identical to the Blue switches, but have a harder spring for a much higher actuation force.',
+  },
+  {
+    color:    'brown',
+    type:     'Tactile Bump',
+    force_g:  45,
+    info:     'The MX Brown switches can be considered a lighter version of the MX Blue switch without the audible click. The tactile bump not as nearly as distinct, but it does still provide enough feedback for your fingers to detect when the switch has been activated.',
+  },
+  {
+    color:    'clear',
+    type:     'Tactile Bump',
+    force_g:  65,
+    info:     'The MX Clear switches can be considered a stronger version of the MX Brown switch. The tactile bump is as more distinct than MX Browns and provides more feedback for your fingers to detect when the switch has been activated.',
+  },
+  {
+    color:    'red',
+    type:     'Linear',
+    force_g:  45,
+    info:     'The MX Red switches are almost identical to the Black switches, but have a softer spring for a much lower actuation force. Although this switch has the same actuation force as the Browns, it feels lighter due to the linear action. Also commonly marketed as a "gamer" switch.',
+  },
+  {
+    color:    'black',
+    type:     'Linear',
+    force_g:  60,
+    info:     'MX Black switches are linear switches, so they have no tactile feedback unless the switch is "bottomed out," meaning the switch is pressed all the way down. The keystroke is smooth all the way down. The stronger spring is said to help the switch reset faster, which can be useful in some instances where "double tapping" is needed. The stiffer switches may also help with accidental key presses from straying fingers. Commonly marketed as a "gamer" switch.',
+  },
+];
